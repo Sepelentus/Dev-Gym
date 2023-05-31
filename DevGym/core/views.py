bb5433f763f91e4f2b2ca883f39842ce968dc615
Sepelentus <vicente.ramirez.gonzalez@gmail.com>
2023-05-31 18:52:16 -0400

    Django creation and homepage advancements

---
+++ DevGym/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def homepage(request):
+    return render(request, 'core/home.html')
+
+def login(request):
+    return render(request, 'core/login.html')
